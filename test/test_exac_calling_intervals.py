3c99e6c21b3cbe29042be6f0c11c62422712202b
bw2 <weisburd@broadinstitute.org>
2015-09-22 15:43:58 -0400

    added tests

---
+++ test/test_exac_calling_intervals.py
@@ -0,0 +1,45 @@
+import os
+import peewee
+import unittest
+from playhouse.test_utils import test_database
+from utils.exac_calling_intervals import *
+
+test_db = peewee.SqliteDatabase(':memory:')
+
+class TestExacCallingIntervals(unittest.TestCase):
+
+    def load_test_data(self):
+        parse_exac_calling_intervals(
+            os.path.join(os.path.dirname(__file__),
+                         "data/calling_regions.interval_list"))
+
+    def test_exac_calling_intervals(self):
+        with test_database(test_db, (ExacCallingInterval,)):
+            self.load_test_data()   # this data will be loaded into `test_db`
+
+            # test that data was loaded correctly
+            intervals = list(ExacCallingInterval.select())
+            self.assertEqual(str(intervals[0]), "1:12141-12277")
+            self.assertEqual(str(intervals[1]), "1:12546-12771")
+            self.assertEqual(str(intervals[-1]), "MT:12287-15937")
+
+            # test get_overlapping_calling_interval
+            intervals = list(ExacCallingInterval.select())
+            self.assertEqual(str(get_overlapping_calling_interval("1", 12546)), "1:12546-12771")
+            self.assertEqual(str(get_overlapping_calling_interval("1", 12671)), "1:12546-12771")
+            self.assertEqual(str(get_overlapping_calling_interval("1", 12771)), "1:12546-12771")
+            self.assertRaises(ValueError, get_overlapping_calling_interval, "1", 12545)
+            self.assertRaises(ValueError, get_overlapping_calling_interval, "1", 12772)
+
+            # test get_adjacent_calling_interval
+            self.assertListEqual(
+                list(map(str, get_adjacent_calling_intervals("1", 12546))),
+                ["1:12141-12277",
+                 "1:12546-12771",
+                 "1:13354-13689"])
+            self.assertListEqual(
+                list(map(str, get_adjacent_calling_intervals("1", 12546, n_left=2, n_right=2))),
+                ["1:12141-12277",
+                 "1:12546-12771",
+                 "1:13354-13689",
+                 "1:17319-17486"])
