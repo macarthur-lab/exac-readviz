e01bc432c6262c42cc8aa1b76fa4c2f05ba26d73
bw2 <weisburd@broadinstitute.org>
2015-09-25 02:54:58 -0400

    Commented out _create_table(..) which is no longer needed

--- utils/database.py
+++ utils/database.py
@@ -102,39 +102,39 @@ class Sample(_SharedVariantFields):
         )
 
 
-def _create_table(db, model, indexes=tuple(), safe=True):
-    """Utility method for creating a database table and indexes that is a
-    work around for unexpected behavior by the peewee ORM module. Specifically,
-    peewee create_table doesn't create compound indexes as expected.
-
-    Args:
-      db: an active peewee database connection
-      model: subclass of peewee.Model
-      indexes: a tuple of indexes that would normally be specified in
-         the peewee.Mode's class Meta. Example:
-
-         indexes = (
-            (('chrom', 'start', 'end'), True),  # True means unique index
-          )
-      safe: if True, no error will be raised if the table already exists
-    """
-    # create table as a compressed TokuDB table
-    raw_query = db.compiler().create_table(model, safe=safe)
-    raw_query = list(raw_query)
-    # this is commented out to avoid
-    #raw_query[0] = raw_query[0] + " engine=TokuDB, compression='tokudb_zlib', charset=latin1"
-    db.execute_sql(*raw_query)
-    logging.debug(raw_query[0])
-
-    # create any indexes
-    safe_str = "IF NOT EXISTS" if safe else ""
-    model_name = model.__name__.lower()
-    for i, (columns, unique) in enumerate(indexes):
-        columns_str = ",".join(map(lambda c: "`"+c+"`", columns))
-        unique_str = "UNIQUE" if unique else ""
-        q = "ALTER TABLE `%(model_name)s` ADD %(unique_str)s KEY %(safe_str)s `index%(i)s`(%(columns_str)s);" % locals()
-        logging.debug(q)
-        db.execute_sql(q)
+#def _create_table(db, model, indexes=tuple(), safe=True):
+#    """Utility method for creating a database table and indexes that is a
+#    work around for unexpected behavior by the peewee ORM module. Specifically,
+#    peewee create_table doesn't create compound indexes as expected.
+#
+#    Args:
+#      db: an active peewee database connection
+#      model: subclass of peewee.Model
+#      indexes: a tuple of indexes that would normally be specified in
+#         the peewee.Mode's class Meta. Example:
+#
+#         indexes = (
+#            (('chrom', 'start', 'end'), True),  # True means unique index
+#          )
+#      safe: if True, no error will be raised if the table already exists
+#    """
+#    # create table as a compressed TokuDB table
+#    raw_query = db.compiler().create_table(model, safe=safe)
+#    raw_query = list(raw_query)
+#    # this is commented out to avoid
+#    #raw_query[0] = raw_query[0] + " engine=TokuDB, compression='tokudb_zlib', charset=latin1"
+#    db.execute_sql(*raw_query)
+#    logging.debug(raw_query[0])
+#
+#    # create any indexes
+#    safe_str = "IF NOT EXISTS" if safe else ""
+#    model_name = model.__name__.lower()
+#    for i, (columns, unique) in enumerate(indexes):
+#        columns_str = ",".join(map(lambda c: "`"+c+"`", columns))
+#        unique_str = "UNIQUE" if unique else ""
+#        q = "ALTER TABLE `%(model_name)s` ADD %(unique_str)s KEY %(safe_str)s `index%(i)s`(%(columns_str)s);" % locals()
+#        logging.debug(q)
+#        db.execute_sql(q)
 
 
 def init_db():
