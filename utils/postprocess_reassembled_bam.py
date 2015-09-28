53b28e0c367cd68ec0bc801a4a8ab7c6dcc8dfce
bw2 <weisburd@broadinstitute.org>
2015-09-28 08:43:52 -0400

    Added empty input bam handling, fixed next_reference_id bug

--- utils/postprocess_reassembled_bam.py
+++ utils/postprocess_reassembled_bam.py
@@ -11,6 +11,12 @@ def postprocess_bam(input_bam_path, output_bam_path):
     obfuscates and downsizes read names, discards all tags (including read groups).
 
     If the input bam doesn't have any reads, the output bam won't be written.
+
+    Return:
+        2-tuple with (is_empty, artificial_haplotype_counter) where
+           is_empty: is True if the input_bam was empty
+           artificial_haplotype_counter: the number of artificial haplotypes found in the input bam
+
     """
 
     # This counter is used as a sanity check that HC added at least one read
@@ -19,57 +25,57 @@ def postprocess_bam(input_bam_path, output_bam_path):
     artificial_haplotype_counter = 0
 
     ibam = pysam.AlignmentFile(input_bam_path, "rb")
-    obam = None
+
+    reference_sequences = []
+    for reference_id in range(len(ibam.header['SQ'])):
+        d = {}
+        reference_sequences.append(d)
+        for key, value in ibam.header['SQ'][reference_id].items():
+            if key in ["SN", "LN"]:
+                d[key] = value
+
+    header = {'HD': { 'VN': '1.4', 'SO': 'coordinate' },
+              'SQ': reference_sequences,
+              'RG': [],
+              }
+    obam = pysam.AlignmentFile(output_bam_path, "wb", header=header)
 
     # iterate over the reads
+    is_input_bam_empty = True
     for r in ibam:
         #if r.get_tag('RG') == "ArtificialHaplotype":  # this doesn't work with the old version of pysam installed on the cluster
         if dict(r.tags).get('RG') == "ArtificialHaplotype":
             artificial_haplotype_counter += 1
             continue
 
-
-        # create the output bam file if necessary
-        if obam is None:
-            chrom_name = ibam.header['SQ'][r.reference_id]['SN']
-            chrom_length = ibam.header['SQ'][r.reference_id]['LN']
-            # put a minimal header with only 1 chrom name
-            header = {'HD': { 'VN': '1.4', 'SO': 'coordinate' },
-                      'SQ': [{'SN': chrom_name, 'LN': chrom_length}],
-                      'RG': [],
-            }
-            obam = pysam.AlignmentFile(output_bam_path, "wb", header=header)
-        else:
-            # check that the current read's reference_id matches the original one
-            current_chrom_name = ibam.header['SQ'][r.reference_id]['SN']
-            assert current_chrom_name == chrom_name, \
-                "File %s contains reads from more than one chromosome: %s, %s" % (
-                    input_bam_path, chrom_name, current_chrom_name)
-
         # copy info from r to s
         s = pysam.AlignedSegment()
         s.query_name = read_name_hash(r.query_name)
         s.query_sequence = r.query_sequence
         s.flag = r.flag
-        s.reference_id = 0  # since the bam should only have reads from one chromosome, there will always be just 1 chromosome entry in the header, and so this reference_id can always be 0.
+        s.reference_id = r.reference_id  # since the bam should only have reads from one chromosome, there will always be just 1 chromosome entry in the header, and so this reference_id can always be 0.
         s.reference_start = r.reference_start
         s.mapping_quality = r.mapping_quality
         s.cigar = r.cigar
-        s.next_reference_id = read_name_hash(r.next_reference_id)
+        s.next_reference_id = r.next_reference_id
         s.next_reference_start = r.next_reference_start
         s.template_length = r.template_length
         s.query_qualities = r.query_qualities
 
         obam.write(s)
 
+        is_input_bam_empty = False
+
     if obam is not None:
         obam.close()
 
+    if not is_input_bam_empty:
         assert artificial_haplotype_counter > 0, \
             "Expected HaplotypeCaller to add at least one record with " \
             "RG == 'ArtificialHaplotype'. " \
             "%(input_bam_path)s => %(output_bam_path)" % locals()
 
+    return (is_input_bam_empty, artificial_haplotype_counter)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser("Takes an HC output bam and discards non-essential header fields and tags, obfuscates read names, etc.")
