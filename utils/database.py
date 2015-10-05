917fefb167c685873a120782ff4f0c83bc617868
bw2 <weisburd@broadinstitute.org>
2015-10-05 15:05:53 -0400

    shorteded column names

--- utils/database.py
+++ utils/database.py
@@ -9,7 +9,9 @@ logging.getLogger('peewee').setLevel(logging.ERROR)
 # define database
 _readviz_db = playhouse.pool.PooledMySQLDatabase(
     'exac_readviz',
-    user='root', host='dmz-exac-dev.broadinstitute.org', port=3307)
+    user='root',
+    host='dmz-exac-dev.broadinstitute.org',
+    port=3307)
 #_readviz_db = peewee.MySQLDatabase('exac_readviz2', user='root', host='dmz-exac-dev.broadinstitute.org', port=3307, autocommit=False)
 #_readviz_db = peewee.SqliteDatabase('exac_readviz.db', autocommit=False)
 #_readviz_db = peewee.SqliteDatabase(':memory:')
@@ -46,9 +48,9 @@ class ExacCallingInterval(_SharedMeta):
 # fields shared by Variant and Sample tables
 class _SharedVariantFields(_SharedMeta):
     chrom = peewee.CharField(max_length=5, null=False, index=True)
-    minrep_pos = peewee.IntegerField()
-    minrep_ref= peewee.CharField(max_length=MAX_ALLELE_SIZE)
-    minrep_alt = peewee.CharField(max_length=MAX_ALLELE_SIZE)
+    pos = peewee.IntegerField()
+    ref= peewee.CharField(max_length=MAX_ALLELE_SIZE)
+    alt = peewee.CharField(max_length=MAX_ALLELE_SIZE)
     het_or_hom = peewee.CharField(max_length=3)
     # finished is 1 if all processing steps for this variant have finished (either successfully or with non-transient errors)
     finished = peewee.BooleanField(default=0)
@@ -67,9 +69,8 @@ class Variant(_SharedVariantFields):
 
     class Meta:
         indexes = (
-            (('chrom', 'minrep_pos', 'minrep_ref', 'minrep_alt', 'het_or_hom'), True), # True means unique index
+            (('chrom', 'pos', 'ref', 'alt', 'het_or_hom'), True), # True means unique index
         )
-
 
 # create table for per-variant-sample info
 # WARNING: this table contains sensitive info (eg. sample ids) and should not
@@ -87,60 +88,66 @@ class Sample(_SharedVariantFields):
     calling_interval_start = peewee.IntegerField(default=None, null=True)
     calling_interval_end = peewee.IntegerField(default=None, null=True)
 
-    hc_started = peewee.BooleanField(default=0, index=True)
     hc_succeeded = peewee.BooleanField(default=0, index=True)
     hc_error_code = peewee.IntegerField(default=None, index=True, null=True)
     hc_error_text = peewee.TextField(default=None, null=True)
     hc_n_artificial_haplotypes = peewee.IntegerField(default=None, index=True, null=True)
+
+    hc_started_time = peewee.DateTimeField(default=None, null=True)
+    hc_finished_time = peewee.DateTimeField(default=None, null=True)
+    hc_command_line = peewee.TextField(default=None, null=True)
 
     #screenshot_started = peewee.BooleanField(default=0)
     #screenshot_finished = peewee.BooleanField(default=0)
 
     class Meta:
         indexes = (
-            (('chrom', 'minrep_pos', 'minrep_ref', 'minrep_alt', 'het_or_hom', 'sample_id'), True), # True means unique index
+            (('chrom', 'pos', 'ref', 'alt', 'het_or_hom', 'sample_id'), True), # True means unique index
         )
 
 
-#def _create_table(db, model, indexes=tuple(), safe=True):
-#    """Utility method for creating a database table and indexes that is a
-#    work around for unexpected behavior by the peewee ORM module. Specifically,
-#    peewee create_table doesn't create compound indexes as expected.
-#
-#    Args:
-#      db: an active peewee database connection
-#      model: subclass of peewee.Model
-#      indexes: a tuple of indexes that would normally be specified in
-#         the peewee.Mode's class Meta. Example:
-#
-#         indexes = (
-#            (('chrom', 'start', 'end'), True),  # True means unique index
-#          )
-#      safe: if True, no error will be raised if the table already exists
-#    """
-#    # create table as a compressed TokuDB table
-#    raw_query = db.compiler().create_table(model, safe=safe)
-#    raw_query = list(raw_query)
-#    # this is commented out to avoid
-#    #raw_query[0] = raw_query[0] + " engine=TokuDB, compression='tokudb_zlib', charset=latin1"
-#    db.execute_sql(*raw_query)
-#    logging.debug(raw_query[0])
-#
-#    # create any indexes
-#    safe_str = "IF NOT EXISTS" if safe else ""
-#    model_name = model.__name__.lower()
-#    for i, (columns, unique) in enumerate(indexes):
-#        columns_str = ",".join(map(lambda c: "`"+c+"`", columns))
-#        unique_str = "UNIQUE" if unique else ""
-#        q = "ALTER TABLE `%(model_name)s` ADD %(unique_str)s KEY %(safe_str)s `index%(i)s`(%(columns_str)s);" % locals()
-#        logging.debug(q)
-#        db.execute_sql(q)
+def _create_table(model, fail_silently=True):
+    """Utility method for creating a database table and indexes that is a
+    work around for unexpected behavior by the peewee ORM module. Specifically,
+    peewee create_table doesn't create compound indexes as expected.
+
+    Args:
+      db: an active peewee database connection
+      model: subclass of peewee.Model
+      indexes: a tuple of indexes that would normally be specified in
+         the peewee.Mode's class Meta. Example:
+
+         indexes = (
+            (('chrom', 'start', 'end'), True),  # True means unique index
+          )
+      fail_silently: if True, no error will be raised if the table already exists
+    """
+    # create table as a compressed TokuDB table
+    db = model._meta.database
+    indexes = model._meta.indexes
+
+    raw_query = db.compiler().create_table(model, safe=fail_silently)
+    raw_query = list(raw_query)
+
+    raw_query[0] = raw_query[0] + " engine=TokuDB, compression='tokudb_zlib', charset=latin1"
+    db.execute_sql(*raw_query)
+    logging.debug(raw_query[0])
+
+    # create indexes
+    safe_str = "IF NOT EXISTS" if fail_silently else ""
+    model_name = model.__name__.lower()
+    for i, (columns, unique) in enumerate(indexes):
+        columns_str = ",".join(map(lambda c: "`"+c+"`", columns))
+        unique_str = "UNIQUE" if unique else ""
+        q = "ALTER TABLE `%(model_name)s` ADD %(unique_str)s KEY %(safe_str)s `index%(i)s`(%(columns_str)s);" % locals()
+        logging.debug(q)
+        db.execute_sql(q)
 
 
 def init_db():
-    ExacCallingInterval.create_table(fail_silently=True)
-    Variant.create_table(fail_silently=True)
-    Sample.create_table(fail_silently=True)
+    _create_table(ExacCallingInterval, fail_silently=True)
+    _create_table(Variant, fail_silently=True)
+    _create_table(Sample, fail_silently=True)
 
     #_readviz_db.connect()
 
