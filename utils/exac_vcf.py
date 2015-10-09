fb39a24eb4e8cd6507b83a262938ed7cf8a92903
bw2 <weisburd@broadinstitute.org>
2015-10-09 09:37:50 -0400

    Fixed to work with both genotypes and sites VCFs

--- utils/exac_vcf.py
+++ utils/exac_vcf.py
@@ -23,7 +23,7 @@ Example usage:
 import logging
 import re
 
-def create_vcf_row_parser(header_line, valid_sample_ids):
+def create_vcf_row_parser(header_line, valid_sample_ids=None):
     """Defines and returns a function that can parse a single VCF row
     Args:
       header_line: The last line of the VCF header - the one that defines columns.
@@ -35,14 +35,16 @@ def create_vcf_row_parser(header_line, valid_sample_ids):
         "Unexpected header_fields 1: %s" % str(header_fields[0:9])
     assert header_fields[3] == "REF" and header_fields[4] == "ALT", \
         "Unexpected header_fields 2: %s" % str(header_fields[0:9])
-    assert header_fields[8] == "FORMAT", \
-        "Unexpected header_fields 3: %s" % str(header_fields[0:9])
+    if valid_sample_ids is not None:
+        assert header_fields[8] == "FORMAT", \
+            "Unexpected header_fields 3: %s" % str(header_fields[0:9])
 
     # sanity check for sample_ids
-    sample_ids = header_fields[9:]
-    for sample_id in sample_ids:
-        if sample_id not in valid_sample_ids:
-            logging.error("ERROR: vcf sample id '%s' is not in the vcf info table" % sample_id)
+    if valid_sample_ids is not None:
+        sample_ids = header_fields[9:]
+        for sample_id in sample_ids:
+            if sample_id not in valid_sample_ids:
+                logging.error("ERROR: vcf sample id '%s' is not in the vcf info table" % sample_id)
 
     expected_genotype_format = "GT:AD:DP:GQ:PL"
     GT_idx = expected_genotype_format.split(":").index("GT")
@@ -66,44 +68,45 @@ def create_vcf_row_parser(header_line, valid_sample_ids):
         assert len(n_het_list) == len(alt_list), "%s:%s: Unexpected number of AC_Het: %s - %s" % (chrom, pos, n_het_list, "\t".join(fields[0:9]))
         assert len(n_hom_list) == len(alt_list), "%s:%s: Unexpected number of AC_Hom: %s - %s" % (chrom, pos, n_hom_list, "\t".join(fields[0:9]))
 
-        assert fields[8] == expected_genotype_format, \
-            "%s:%s: Unexpected genotype format: '%s'. Expected: %s in %s" % (chrom, pos, fields[8], expected_genotype_format, fields[0:8])
-
-        genotypes = fields[9:]
-        assert len(sample_ids) == len(genotypes), \
-            "%s:%s: Unexpected num sample ids (%s) vs num genotypes (%s) in %s" % (chrom, pos, len(sample_ids), len(genotypes), fields[0:8])
-
         sample_id_to_genotype = {}
-        for sample_id, genotype in zip(sample_ids, genotypes):
-            genotype_values = genotype.split(":")
-            GT = genotype_values[GT_idx]
-            if GT == "./.":
-                gt_ref = gt_alt = None
-                GQ = DP = None
-            else:
-                gt_ref, gt_alt = GT.split("/")
-                try:
-                    gt_ref = int(gt_ref)
-                    gt_alt = int(gt_alt)
+        if valid_sample_ids is not None:
+            assert fields[8] == expected_genotype_format, \
+                "%s:%s: Unexpected genotype format: '%s'. Expected: %s in %s" % (chrom, pos, fields[8], expected_genotype_format, fields[0:8])
 
-                except ValueError:
-                    logging.error("ERROR: %s:%s - couldn't parse genotype %s in %s" % (chrom, pos, GT, fields[0:8]))
+            genotypes = fields[9:]
+            assert len(sample_ids) == len(genotypes), \
+                "%s:%s: Unexpected num sample ids (%s) vs num genotypes (%s) in %s" % (chrom, pos, len(sample_ids), len(genotypes), fields[0:8])
 
-                assert gt_ref <= len(alt_list) and gt_alt <= len(alt_list), "ERROR: %s:%s - genotype numbers %s out of bounds in %s" % (chrom, pos, GT, fields[0:8])
 
-                try:
-                    GQ = float(genotype_values[GQ_idx])
-                    DP = float(genotype_values[DP_idx])
-                except ValueError:
-                    logging.error("ERROR: %s:%s - couldn't parse %s genotype: %s in %s" % (chrom, pos, sample_id, genotype, fields[0:8]))
-                    # This error happens for genotypes like 1/1:0,0:.:6:70,6,0.
-                    # set GQ, DP = 0 so this sample will be filtered out by the GQ<20,DP<10 filter
-                    GQ = DP = 0
+            for sample_id, genotype in zip(sample_ids, genotypes):
+                genotype_values = genotype.split(":")
+                GT = genotype_values[GT_idx]
+                if GT == "./.":
+                    gt_ref = gt_alt = None
+                    GQ = DP = None
+                else:
+                    gt_ref, gt_alt = GT.split("/")
+                    try:
+                        gt_ref = int(gt_ref)
+                        gt_alt = int(gt_alt)
 
-            sample_id_to_genotype[sample_id] = (gt_ref, gt_alt, GQ, DP)
+                    except ValueError:
+                        logging.error("ERROR: %s:%s - couldn't parse genotype %s in %s" % (chrom, pos, GT, fields[0:8]))
+
+                    assert gt_ref <= len(alt_list) and gt_alt <= len(alt_list), "ERROR: %s:%s - genotype numbers %s out of bounds in %s" % (chrom, pos, GT, fields[0:8])
+
+                    try:
+                        GQ = float(genotype_values[GQ_idx])
+                        DP = float(genotype_values[DP_idx])
+                    except ValueError:
+                        logging.error("ERROR: %s:%s - couldn't parse %s genotype: %s in %s" % (chrom, pos, sample_id, genotype, fields[0:8]))
+                        # This error happens for genotypes like 1/1:0,0:.:6:70,6,0.
+                        # set GQ, DP = 0 so this sample will be filtered out by the GQ<20,DP<10 filter
+                        GQ = DP = 0
+
+                sample_id_to_genotype[sample_id] = (gt_ref, gt_alt, GQ, DP)
 
 
         return chrom, pos, ref, alt_list, n_het_list, n_hom_list, sample_id_to_genotype
 
     return vcf_row_parser
-
