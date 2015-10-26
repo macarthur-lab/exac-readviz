8584612823bcf6468b3e68b9d41a62748f56d64f
bw2 <weisburd@broadinstitute.org>
2015-10-26 14:35:01 -0400

    added chrY

--- scripts/submit_combine.sh
+++ scripts/submit_combine.sh
@@ -5,7 +5,7 @@ cd /home/unix/weisburd/code/exac_readviz_scripts/scripts/
 d='/broad/hptmp/exac_readviz_backend2'
 
 
-for chrom in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X;
+for chrom in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y;
 do
 	qsub -q long -o ${d}/logs/combine_chr${chrom}.log -cwd -j y -V ./run_combine_chrom.sh $chrom
 done;
