37b608d6496a09459ce10033d398740fa8bf2f22
bw2 <weisburd@broadinstitute.org>
2016-04-21 23:27:44 -0400

    Fixed hemizygous sample logic to better match Monkol's calculation

--- utils/choose_samples.py
+++ utils/choose_samples.py
@@ -21,6 +21,12 @@ def _is_male(sex):
         raise ValueError("Unexpected value for sex arg: %s" % sex)
 
 
+def _is_hemizygous(chrom, pos, sex):
+    """Utility method that takes chrom (eg. '1', 'X', 'Y', etc.), position as an integer, and sex as either "m" or "f"
+    and returns True if this position should be considered hemizygous"""
+    return chrom in ('X', 'Y') and _is_male(sex) and _is_hemizygous_segment(chrom, pos)
+
+
 def best_for_readviz_sample_id_iter(chrom, pos, het_or_hom_or_hemi, alt_allele_index, genotypes, sample_id_include_status, sample_id_sex):
     """Implements heuristics for choosing which samples are best to display for
     a given variant (chrom, pos, ref, alt) in it's het or hom-alt state.
@@ -43,52 +49,60 @@ def best_for_readviz_sample_id_iter(chrom, pos, het_or_hom_or_hemi, alt_allele_i
     """
     assert het_or_hom_or_hemi in ["het", "hom", "hemi"], "Unexpected het_or_hom_or_hemi arg: %s" % het_or_hom_or_hemi
 
-    #counter = OrderedDict()
+    counter = OrderedDict()
     
     # filter all samples down to just samples that have the desired genotype and have include=YES
     relevant_samples = []  # a list of dicts
     for sample_id, (gt_ref, gt_alt, AD, DP, GQ) in genotypes.items():
-        #counter['total'] = counter.get('total', 0) + 1
+        counter['total'] = counter.get('total', 0) + 1
         if gt_ref is None and gt_alt is None:
             continue
-        #counter['has_genotype'] = counter.get('has_genotype', 0) + 1
+        counter['has_genotype'] = counter.get('has_genotype', 0) + 1
 
         if het_or_hom_or_hemi == "het":
             if gt_ref == gt_alt:
                 continue  # skip if homozygous
             if gt_ref != alt_allele_index and gt_alt != alt_allele_index:
                 continue # skip if neither allele matches the specific alt allele we're looking for (eg. 1/3)
+            if _is_hemizygous(chrom, pos, sample_id_sex[sample_id]):
+                continue
         elif het_or_hom_or_hemi == "hom":
             if gt_ref != gt_alt:
                 continue  # skip unless homozygous
             if gt_alt != alt_allele_index:
                 continue  # skip unless homozygous for the specific alt allele we're looking for (this matters for multiallelics)
-            if chrom in ('X', 'Y') and _is_male(sample_id_sex[sample_id]) and _is_hemizygous_segment(chrom, pos):
+            if _is_hemizygous(chrom, pos, sample_id_sex[sample_id]):
                 continue
         elif het_or_hom_or_hemi == "hemi" and chrom in ('X', 'Y'):
-            #counter['hemizygous'] = counter.get('hemizygous', 0) + 1
-            if not _is_male(sample_id_sex[sample_id]) or not _is_hemizygous_segment(chrom, pos):
+            if gt_ref != alt_allele_index and gt_alt != alt_allele_index:
+                continue # skip if neither allele matches the specific alt allele we're looking for (eg. 1/3)
+            counter['correct allele'] = counter.get('correct allele', 0) + 1
+
+            if not _is_hemizygous(chrom, pos, sample_id_sex[sample_id]):
                 continue
-            #counter['sex is male and hemizigous_segment'] = counter.get('sex is male and hemizigous_segment', 0) + 1
+            counter['sex is male and hemizigous_segment'] = counter.get('sex is male and hemizigous_segment', 0) + 1
 
             # check whether the allele with the most reads is the ref allele, in which case it's homozygous reference
-            if max(AD) == AD[0]:
+            if max(AD) not in AD[1:]:
                 continue
+            counter['AD supports alt allele'] = counter.get('AD supports alt allele', 0) + 1
         else:
             raise ValueError("Unexpected het_or_hom_or_hemi value: " + str(het_or_hom_or_hemi))
 
         if DP < 10 or GQ < 20:
             continue  # skip samples that don't pass _Adj thresholds since they are not counted in the ExAC browser het/hom counts.
-        #counter["passes GQ, DP"] = counter.get('passes GQ, DP', 0) + 1
+        counter["passes GQ, DP"] = counter.get('passes GQ, DP', 0) + 1
         
         if not sample_id_include_status[sample_id]:
             continue  # skip samples where include status != "YES"
+        counter["include"] = counter.get('include', 0) + 1
 
-        #counter["passes include"] = counter.get('passes include', 0) + 1
         relevant_samples.append( {"sample_id": sample_id, "GQ": GQ} )
 
-    #for k in counter:
-    #    print("%s: %s" % (counter[k], k))
+    #   print(sample_id, (gt_ref, gt_alt, AD, DP, GQ))
+    #if het_or_hom_or_hemi == "hemi":
+    #    for k in counter:
+    #        print("%s: %s" % (counter[k], k))
 
     # return sample ids in order from highest to lowest GQ.
     return [sample["sample_id"] for sample in sorted(relevant_samples, key=lambda s: s["GQ"], reverse=True)]
