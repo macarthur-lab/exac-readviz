f8ffbb60a19f7af3021563c54c672d2cb14e1cff
bw2 <weisburd@broadinstitute.org>
2016-06-20 07:27:59 -0400

    aded pip install

--- lauch_jobs_to_process_variants_table.sh
+++ lauch_jobs_to_process_variants_table.sh
@@ -1,11 +1,17 @@
 export TERM=xterm
 source /broad/software/scripts/useuse
+
 use Java-1.7
 use UGER
+use Python-2.7
+use Samtools
+
+pip install -r requirements.txt --user --upgrade
+
 
 cd /home/unix/weisburd/code/exac_readviz_scripts;
 
-qsub -t 1-5000 \
+qsub -t 1-1000 \
 -o /broad/hptmp/exac_readviz_backend/logs -e /broad/hptmp/exac_readviz_backend/logs \
 -cwd -j y -V  -q short \
 ./run_python.sh python generate_HC_bams.py \
