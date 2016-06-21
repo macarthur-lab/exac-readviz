48e678305a73abb44a9085291a01a12750d6a968
bw2 <weisburd@broadinstitute.org>
2016-06-21 10:17:14 -0400

    utility scripts

---
+++ scripts/reset_variant_and_sample_tables.sql
@@ -0,0 +1,4 @@
+update variant set started=0, started_time=NULL, finished=0, finished_time=NULL, comments=NULL, username=NULL; 
+drop table sample;
+
+#update sample set started=0, started_time=NULL, finished=0, finished_time=NULL, original_bam_path=NULL, original_gvcf_path=NULL, output_bam_path=NULL, hc_succeeded=0, hc_error_text=NULL, hc_error_code=NULL, hc_command_line=NULL, sample_id='', comments=NULL;
