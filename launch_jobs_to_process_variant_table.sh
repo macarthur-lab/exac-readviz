6be494b4d490a782de1aa13bfb30b8a5c37ed8fc
bw2 <weisburd@broadinstitute.org>
2016-08-19 07:15:41 -0400

    added pip install --upgrade pip

--- launch_jobs_to_process_variant_table.sh
+++ launch_jobs_to_process_variant_table.sh
@@ -6,13 +6,15 @@ use UGER
 use Python-2.7
 use Samtools
 
-pip install -r requirements.txt --user --upgrade
+echo "Launching array with $1 tasks";
 
+pip install --user --upgrade pip wheel
 
-cd /home/unix/weisburd/code/exac_readviz_scripts;
+cd /home/unix/weisburd/code/exac_readviz_scripts && \
+pip install -r requirements.txt --user --upgrade  && \
+python2.7 -u parallelize.py -isize 1000000 -n $1 -L /home/unix/weisburd/code/exac_readviz_scripts/scripts/data/25_hc_runs.intervals python2.7 -u -m pipeline.compute_HC_bams_from_sample_table
 
-qsub -t 1-1000 \
--o /broad/hptmp/exac_readviz_backend/logs -e /broad/hptmp/exac_readviz_backend/logs \
--cwd -j y -V  -q short \
-./run_python.sh python generate_HC_bams.py \
---process-variant-table  ` if [ "$#" -ne 0 ]; then echo --chrom $1; fi `
+# /seq/references/Homo_sapiens_assembly19/v1/variant_calling/exome_calling_regions.v1.interval_list
+#qsub -t 1-1000 -o /broad/hptmp/exac_readviz_backend/logs -e /broad/hptmp/exac_readviz_backend/logs -cwd -j y -V  -q short \
+#./run_python.sh python generate_HC_bams.py --process-variant-table  ` if [ "$#" -ne 0 ]; then echo --chrom $1; fi `
+
