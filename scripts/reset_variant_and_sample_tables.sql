f0046eb0a25e8302dd482021884c2df2836c85d1
bw2 <weisburd@broadinstitute.org>
2016-08-19 07:18:13 -0400

    commented out update sample

--- scripts/reset_variant_and_sample_tables.sql
+++ scripts/reset_variant_and_sample_tables.sql
@@ -1,5 +1,6 @@
 update variant set started=0, started_time=NULL, finished=0, finished_time=NULL, comments=NULL, username=NULL, n_available_samples=0, readviz_bam_paths=NULL;
-drop table sample;
+# drop table sample;
 
+#  reset all sample records 
+# update sample set started=0, started_time=NULL, finished=0, finished_time=NULL, original_bam_path=NULL, original_gvcf_path=NULL, output_bam_path=NULL, hc_succeeded=0, hc_error_text=NULL, hc_error_code=NULL, hc_command_line=NULL, sample_id='', comments=NULL;
 
-#update sample set started=0, started_time=NULL, finished=0, finished_time=NULL, original_bam_path=NULL, original_gvcf_path=NULL, output_bam_path=NULL, hc_succeeded=0, hc_error_text=NULL, hc_error_code=NULL, hc_command_line=NULL, sample_id='', comments=NULL;
