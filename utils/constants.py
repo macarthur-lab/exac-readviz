8dc40f39708aa76b2a81147fc5d823543fe785e5
bw2 <weisburd@broadinstitute.org>
2016-09-30 10:52:35 -0400

    updated paths for exac v2

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
-B0;95;0cimport logging
+import logging
 import os
 
 from utils.file_utils import does_file_exist
@@ -19,7 +19,7 @@ MAX_SAMPLES_TO_SHOW_PER_VARIANT = 5
 BACKUP_SAMPLES_IN_CASE_OF_ERRORS = 5
 
 # in ExAC v3 the max ref allele is 325bp long, and max alt allele is 365bp long, so use 500 to be safe.
-MAX_ALLELE_SIZE = 500  # nucleotides
+MAX_ALLELE_SIZE = 350  # nucleotides
 
 # in the ExAC v3 info table, the longest sample id is 36 chars long
 MAX_VCF_SAMPLE_ID_SIZE = 50
@@ -32,11 +32,15 @@ NUM_OUTPUT_DIRECTORIES_L1 = 1000
 INCLUDE_N_ADJACENT_CALLING_REGIONS = 2
 
 EXAC_CALLING_INTERVALS_PATH = os.path.join(DATA_DIR_PREFIX, "exome_calling_regions.v1.interval_list")
+GENCODE_EXAC_GTF_PATH = os.path.join(DATA_DIR_PREFIX, "gencode.gtf.gz")
+
+HUMAN_GENOME_FASTA_PATH = os.path.join("/seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta")
+
 #EXAC_INFO_TABLE_PATH = os.path.join(DATA_DIR_PREFIX, "ExAC.r0.3_meta_Final.tsv")
 #EXAC_POP_SEX_TABLE_PATH = os.path.join(DATA_DIR_PREFIX, "samples_pop_sex.tsv")
 #EXAC_FULL_VCF_PATH = os.path.join(DATA_DIR_PREFIX, "exac_all.vcf.gz")
 #EXAC_SITES_VCF_PATH = os.path.join(DATA_DIR_PREFIX, "ExAC.r0.3.1.sites.vep.vcf.gz")
-GENCODE_EXAC_GTF_PATH = os.path.join(DATA_DIR_PREFIX, "gencode.gtf.gz")
+
 
 PICARD_JAR_PATH = os.path.join(BIN_DIR_PREFIX,"picard.jar")  # used for sorting bam
 GATK_JAR_PATH = "/humgen/gsa-hpprojects/GATK/bin/GenomeAnalysisTK-3.4-46-gbc02625/GenomeAnalysisTK.jar"
@@ -58,9 +62,9 @@ IGV_SCREEN_WIDTH = 300
 IGV_TRACK_HEIGHT = 500
 
 all_files_exist = True
-for path in (EXAC_CALLING_INTERVALS_PATH, EXAC_INFO_TABLE_PATH,
-             EXAC_POP_SEX_TABLE_PATH, EXAC_FULL_VCF_PATH, EXAC_SITES_VCF_PATH,
-             EXAC_SITES_VCF_PATH, GENCODE_EXAC_GTF_PATH,
+for path in (EXAC_CALLING_INTERVALS_PATH, #EXAC_INFO_TABLE_PATH,
+             #EXAC_POP_SEX_TABLE_PATH, EXAC_FULL_VCF_PATH, EXAC_SITES_VCF_PATH,
+             GENCODE_EXAC_GTF_PATH, #EXAC_SITES_VCF_PATH, 
              PICARD_JAR_PATH, GATK_JAR_PATH,
              TCGA_NEW_BAM_PATHS):
     if not does_file_exist(path, use_cache=False):
