5a45215bb9a61fd22d17c74ad587ec42ce0b2909
bw2 <weisburd@broadinstitute.org>
2016-10-13 18:12:59 -0400

    updated intervals file

--- launch_jobs_to_process_variant_table.sh
+++ launch_jobs_to_process_variant_table.sh
@@ -14,7 +14,7 @@ pip install --user --upgrade pip wheel
 
 cd /home/unix/weisburd/code/exac_readviz_scripts && \
 pip install -r requirements.txt --user --upgrade  && \
-python2.7 -u parallelize.py -isize 1000000 -n $1 -L /home/unix/weisburd/code/exac_readviz_scripts/scripts/data/25_hc_runs.intervals python2.7 -u -m pipeline.compute_HC_bams_from_sample_table
+python2.7 -u parallelize.py -isize 1000000 -n $1 -L /home/unix/weisburd/code/exac_readviz_scripts/scripts/data/25bp_exome_calling_regions.v1.interval_list python2.7 -u -m pipeline.compute_HC_bams_from_sample_table
 
 # /seq/references/Homo_sapiens_assembly19/v1/variant_calling/exome_calling_regions.v1.interval_list
 #qsub -t 1-1000 -o /broad/hptmp/exac_readviz_backend/logs -e /broad/hptmp/exac_readviz_backend/logs -cwd -j y -V  -q short \
