7e15b1a447628d3504eaf62b7f50c71ae1569a8b
bw2 <weisburd@broadinstitute.org>
2017-03-14 16:14:39 -0400

    set umask

--- launch_jobs_to_process_variant_table.sh
+++ launch_jobs_to_process_variant_table.sh
@@ -8,14 +8,18 @@ reuse UGER
 reuse Python-2.7
 reuse Samtools
 
-echo "Launching array with $1 tasks";
+NUM_TASKS=$1 # 60000
+echo "Launching array with $NUM_TASKS tasks";
+
+umask 000
 
 pip install --user --upgrade pip wheel
 
 cd /home/unix/weisburd/code/exac_readviz_scripts && \
 pip install -r requirements.txt --user --upgrade  && \
-python2.7 -u parallelize.py -isize 1000000 -n $1 -L /home/unix/weisburd/code/exac_readviz_scripts/scripts/data/25bp_exome_calling_regions.v1.interval_list python2.7 -u -m pipeline.compute_HC_bams_from_sample_table
+python2.7 -u parallelize.py -isize 1000000 -n $NUM_TASKS -L /home/unix/weisburd/code/exac_readviz_scripts/scripts/data/gencode_exons_plus_75bp_split_into_25bp.sorted.merged.intervals python2.7 -u -m pipeline.compute_HC_bams_from_sample_table
 
+# /home/unix/weisburd/code/exac_readviz_scripts/scripts/data/25bp_exome_calling_regions.v1.interval_list
 # /seq/references/Homo_sapiens_assembly19/v1/variant_calling/exome_calling_regions.v1.interval_list
 #qsub -t 1-1000 -o /broad/hptmp/exac_readviz_backend/logs -e /broad/hptmp/exac_readviz_backend/logs -cwd -j y -V  -q short \
 #./run_python.sh python generate_HC_bams.py --process-variant-table  ` if [ "$#" -ne 0 ]; then echo --chrom $1; fi `
